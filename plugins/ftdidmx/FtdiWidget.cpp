0679e76750e50ac9b64a6fc7e78b033f50e6dc78
Daniel Harsojo <daniel@harsojo.id.au>
2015-04-01 18:22:17 +1100

    Adding missing OR

--- plugins/ftdidmx/FtdiWidget.cpp
+++ plugins/ftdidmx/FtdiWidget.cpp
@@ -178,7 +178,7 @@ void FtdiWidget::Widgets(vector<FtdiWidgetInfo> *widgets) {
         if (std::string::npos != v.find("FTDI") ||
             std::string::npos != v.find("KMTRONIC") ||
             std::string::npos != v.find("KWMATIK") ||
-            std::string::npos != v.find("WWW.SOH.CZ")
+            std::string::npos != v.find("WWW.SOH.CZ") ||
             std::string::npos != v.find("DMXKING.COM")) {
           widgets->push_back(FtdiWidgetInfo(sname,
                                             sserial,
